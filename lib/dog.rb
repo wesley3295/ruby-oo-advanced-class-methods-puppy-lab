617ce6641144fe06db4ca91e2df12a488ab9653d
Wesley <wspencer3295@yahoo.com>
2020-11-18 14:58:09 -0700

    commit tree b6e22648aa4ca29f780a28b898f7ec78ea44d370

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+    attr_reader 
+
+    @@all = []
+    
+    def initialize(name)
+    @name = name
+    save
+end
+
+def self.all
+@@all
+end
+
+def self.print_all
+    @@all.collect do |dog|
+       puts dog.name
+    end
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+    def save
+        @@all << self
+    end
+end
